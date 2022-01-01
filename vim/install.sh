142ca43349fb06ce307342b8ba7a961d1f3bd51e
Maxime Bouillot <maxime.bouillot@withings.com>
2022-01-01 18:21:40 +0100

    [GLOBAL][VIM] Added rainbow parentheses.

--- vim/install.sh
+++ vim/install.sh
@@ -2,19 +2,19 @@
 
 /bin/rm -rfv bundle
 
-#CSCOPE
+# CSCOPE
 git clone https://github.com/Arkaeriit/cscope_map_fork.git
 mkdir -p bundle/cscope/plugin
 mv cscope_map_fork/* bundle/cscope/plugin
 rm -Rf cscope_map_fork
 
-#Killersheep
+# Killersheep
 git clone https://github.com/vim/killersheep.git
 mkdir -p bundle/killersheep/plugin
 mv killersheep/plugin/* bundle/killersheep/plugin/
 rm -Rf killersheep
 
-#YCP
+# YCP
 mkdir -p bundle
 cd bundle
 git clone https://github.com/ycm-core/YouCompleteMe.git
@@ -23,8 +23,13 @@ git submodule update --init --recursive
 python3 install.py
 cd ../..
 
-#NerdCommenter
+# NerdCommenter
 cd bundle
 git clone https://github.com/preservim/nerdcommenter.git
+cd ..
+
+# Rainbow Parentheses
+cd bundle
+git clone https://github.com/kien/rainbow_parentheses.vim.git
 cd ..
 

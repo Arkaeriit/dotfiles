03789d69ece54d3c567a1b121e74956cb79889d9
Maxime Bouillot <maxime.bouillot@withings.com>
2022-02-22 16:37:20 +0100

    [GLOBAL][FISH] Made prompt smaller if needed.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,18 +1,44 @@
 function fish_prompt
+    # Symbols definition
     set prev_status $status
     set main_color cyan
     set sec_color blue
     set error_color red
     set smile '^[n.n]^'
-    set -x $fish_prompt_pwd_dir_length 0 #permet de ne pas raccoursir le nom des dossiers
+    set -x $fish_prompt_pwd_dir_length 0 # permet de ne pas raccoursir le nom des dossiers
+    set __pwd (prompt_pwd_full)" "
+
+    # Checking the size of the prompt and removing excessive elements
+    set long_prompt "$smile $__pwd $fish_chev_vi >"
+    set len_prompt (string length "$long_prompt")
+    if test $len_prompt -ge $COLUMNS
+        set __pwd ""
+        set smaler_prompt "$smile $fish_chev_vi >"
+        set len_prompt (string length "$smaler_promt")
+        if test $len_prompt -ge $COLUMNS
+            set smile ""
+            set smol_prompt "$fish_chev_vi >"
+            set len_prompt (string length "$smol_promt")
+            if test $len_prompt -ge $COLUMNS
+                set fish_chev_vi ""
+            end
+        end
+    end
+    
+    # Printing smile
     set_color -o $sec_color 
     echo -n $smile
-    echo -n ' '
+
+    # Printing working directory
     set_color -o $main_color
-    echo -n (prompt_pwd_full) 
+    #echo -n (prompt_pwd_full) 
+    echo -n "$__pwd"
+
+    # Printing Vi-mode hint
     set_color -o $sec_color
-    echo -n ' '
     echo -n $fish_chev_vi #variable moddified by fish_mode_promp.fish to tell the vi mode
+
+    # Printing colored chevron
     if test $prev_status -eq 0 #color evolutive promp
         set_color $main_color
     else

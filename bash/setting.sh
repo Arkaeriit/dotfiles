7875ac61c5dab4ee6c88ee81b514cf71373eedfc
Maxime Bouillot <maxime.bouillot@withings.com>
2022-03-22 12:11:50 +0100

    [GLOBAL][SHELL] Changed the config of the pager.
    It does not show line number but it show input file.

--- bash/setting.sh
+++ bash/setting.sh
@@ -2,6 +2,7 @@
 
 export EDITOR='vim'
 export VISUAL='vim'
+export PAGER='bat --tabs 4 --style=header,grid --decorations auto --color always --paging auto --theme Nord --wrap never'
 CXX=/usr/bin/g++
 export CXX
 

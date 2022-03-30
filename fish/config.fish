02ed2f567f309690fdb12203d833dea3cdd0733d
Maxime Bouillot <maxime.bouillot@withings.com>
2022-03-30 09:37:32 +0200

    [GLOBAL][SHELL] Added more ..... as cd.

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,14 @@
 source ~/.config/fish/variables.fish
 source ~/.config/fish/setting.fish
 source ~/.config/fish/startup.fish
+
 alias hd='hexyl'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .......='cd ../../../../../..'
+alias ........='cd ../../../../../../..'
+
 

0fe72130927ae6af708c4910883f170378d79133
Maxime Bouillot <maxime.bouillot@withings.com>
2022-04-26 13:49:25 +0200

    [GLOBAL][BASH] Made the pager selection from Fish in Bash.

--- bash/setting.sh
+++ bash/setting.sh
@@ -2,7 +2,18 @@
 
 export EDITOR='vim'
 export VISUAL='vim'
-export PAGER='bat --tabs 4 --style=header,grid --decorations auto --color always --paging auto --theme Nord --wrap never'
 CXX=/usr/bin/g++
 export CXX
+
+if command -v bat > /dev/null
+then
+    export PAGER='bat --tabs 4 --style=header,grid --decorations auto --color always --paging auto --theme Nord --wrap never'
+else
+    if command -v less > /dev/null
+    then
+        export PAGER='less'
+    else
+        export PAGER='more'
+    fi
+fi
 

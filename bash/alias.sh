e8b550218df1844932688ecea6192491c8f61ed3
Maxime Bouillot <maxime.bouillot@withings.com>
2022-04-26 14:00:16 +0200

    [GLOBAL][SHELL] "pager" alias can run "$PAGER".

--- bash/alias.sh
+++ bash/alias.sh
@@ -39,6 +39,7 @@ alias :q="exit"
 alias sayFR="espeak "$argv" -g 5 -s 130 --stdout -v fr | aplay" # Text to speech en français
 alias pastable-csv='sed -e "s/,/\t/g" ' # This quick sed transform makes a csv into something pastable on Google sheet. This should be used along with the copy alias.
 alias dot-to-comma='sed "s/\./\,/g"'
+alias pager="$PAGER" # Run the PAGER environement variable
 
 
 # Convert the file given as argument into a mp3 file
---
+++ fish/functions/pager.fish
@@ -0,0 +1,6 @@
+# Runs the content of the environment variable PAGER
+
+function pager
+    $PAGER $argv
+end
+

b0f3720bee52a129bcb27942eaa99eb5a71393d1
Maxime Bouillot <maxime.bouillot@withings.com>
2023-04-13 10:45:01 +0200

    [LOCAL][SHELL] Fix eddit_addr regex.

--- fish/functions/edit_addr.fish
+++ fish/functions/edit_addr.fish
@@ -2,7 +2,7 @@
 function edit_addr -d "Edit an address from an embedded firmware"
     if test (count $argv) -gt 0
         if arm-none-eabi-addr2line $argv[1] -e objs/app.elf > /dev/null 2> /dev/null
-            arm-none-eabi-addr2line $argv[1] -e objs/app.elf | sed "s/:/ +/" | xargs vim
+            arm-none-eabi-addr2line $argv[1] -e objs/app.elf | sed "s: .*::" | sed "s/:/ +/" | xargs vim
         else
             echo "Elf file or address invalid." > /dev/stderr
             return 2

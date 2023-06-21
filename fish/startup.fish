fe3a143dd4b93653d5b347b0776a43a30709593b
Maxime Bouillot <maxime.bouillot@withings.com>
2023-06-21 09:41:25 +0200

    [LOCAL][FISH] Appropriate minicom alias.

--- fish/startup.fish
+++ fish/startup.fish
@@ -1,7 +1,7 @@
 #!/usr/local/bin/fish
 
 ASCnotes read 21600
-alias minicom='minicom -con -R utf-8 '
+alias minicom='minicom -con -R utf-8 -D /dev/ttyUSB0 -b 1000000 '
 unset fish_chev_vi
 
 # Ensure fish doen't repport itself as bash

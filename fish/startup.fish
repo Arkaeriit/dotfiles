4a5a1b8febcf8d8c4597a57139ad50a87fae8762
Maxime Bouillot <maxime.bouillot@legrand.com>
2024-05-24 11:19:36 +0200

    [LOCAL][FISH] Export Netatmo password for compilation.

--- fish/startup.fish
+++ fish/startup.fish
@@ -4,3 +4,12 @@ ASCnotes read 21600
 alias minicom='minicom -con -R utf-8 -D /dev/ttyUSB0 -b 115200 '
 unset fish_chev_vi
 
+if [ -z $KEY_MOUNTED ]
+    set -Ux KEY_MOUNTED yep
+    #$NETATMO_WORK_DIR/embedded/modules/magellan-keys/pass_tool.sh -m
+    set -Ux PASSWORD_STORE_DIR "$NETATMO_WORK_DIR/embedded/modules/magellan-keys"
+    for key in (pass find NTM | grep -v : | sed 's:[^A-Z_]::g')
+        set -Ux $key (pass $key)
+    end
+end
+

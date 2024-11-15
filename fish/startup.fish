6dfb193020a79d99dde9ace1f29263577c09261b
Maxime Bouillot <maxime.bouillot@legrand.com>
2024-11-15 10:42:27 +0100

    [LOCAL][FISH] Remove janky password management.

--- fish/startup.fish
+++ fish/startup.fish
@@ -8,16 +8,6 @@ unset fish_chev_vi
 unset SHELL
 set -Ux SHELL (which fish)
 
-# Add Netatmo compilation passwords
-if [ -z $KEY_MOUNTED ]
-    set -Ux KEY_MOUNTED yep
-    #$NETATMO_WORK_DIR/embedded/modules/magellan-keys/pass_tool.sh -m
-    set -Ux PASSWORD_STORE_DIR "$NETATMO_WORK_DIR/embedded/modules/magellan-keys"
-    for key in (pass find NTM | grep -v : | sed 's:[^A-Z_]::g')
-        set -Ux $key (pass $key)
-    end
-end
-
 # Ensure fish doen't repport itself as bash
 unset SHELL
 set -Ux SHELL (which fish)

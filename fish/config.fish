77457405f06d7a3705a3228d44ddf4b81b714c32
Maxime Bouillot <maxbouillot@gmail.com>
2025-01-06 11:20:58 +0100

    [LOCAL][FISH] Add pnpn
    It's used to run static analysis results display.

--- fish/config.fish
+++ fish/config.fish
@@ -12,3 +12,10 @@ alias ......='cd ../../../../..'
 alias .......='cd ../../../../../..'
 alias ........='cd ../../../../../../..'
 
+
+# pnpm
+set -gx PNPM_HOME "/home/mbouillot/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
